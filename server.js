163c87c289a3b8c42ec0f1b0c37dfd5ee975b159
wingching <devfast212@gmail.com>
2022-07-25 19:45:10 +0900

    Merge backend and frontend'

--- server.js
+++ server.js
@@ -2,47 +2,50 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const compression = require('compression')
 const cors = require('cors')
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const key = require('./config/key');
-const passport = require('passport');
-const timer = require('./timer');
+const mongoose = require('mongoose')
+const helmet = require('helmet')
+const key = require('./config/key')
+const passport = require('passport')
+const timer = require('./timer')
+const path = require('path')
 
-require('./config/passport');
+require('./config/passport')
 
 // Import routes
-const authRouter = require('./routes/auth.route');
-const accountRouter = require('./routes/account.route');
+const authRouter = require('./routes/auth.route')
+const accountRouter = require('./routes/account.route')
 
 const app = express()
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.use(cors())
 app.use(helmet())
 app.use(compression())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(passport.initialize());
+app.use(passport.initialize())
 
 // Implement route
 app.use('/auth', authRouter)
 app.use('/nfttrading', accountRouter)
 
 // Implement 500 error route
-app.use(function(err, req, res, next) {
-    res.status(500).send('Something is broken.')
+app.use(function (err, req, res, next) {
+  res.status(500).send('Something is broken.')
 })
 
 // Implement 404 error route
-app.use(function(req, res, next) {
-    res.status(404).send('Sorry we could not find that.')
+app.use(function (req, res, next) {
+  res.status(404).send('Sorry we could not find that.')
 })
 
-mongoose.connect(process.env.MONGOLAB_URL || key.MONGO_URL)
-.then(() => console.log('mongoose connected.'))
-.catch((err) => console.log('mongoose error: ' + err));
+mongoose
+  .connect(process.env.MONGOLAB_URL || key.MONGO_URL)
+  .then(() => console.log('mongoose connected.'))
+  .catch((err) => console.log('mongoose error: ' + err))
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001
 
-timer.run();
+timer.run()
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
