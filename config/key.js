603e72781c60910418bb027175f4ccf245542899
wingching <devfast212@gmail.com>
2022-07-25 10:10:02 +0900

    finish backend

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URL: "mongodb://localhost:27017/",
+    // MONGO_URL: "mongodb+srv://Edgars714:gYrm7fxC0xcdfFyo@cluster0.swkkimq.mongodb.net/mint",
+    SECRET: "jwt-secret",
+}
