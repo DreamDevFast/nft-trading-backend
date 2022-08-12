dc8f36a1346c6f5c1d930d575c6c23eb5bd00ae4
wingching <devfast212@gmail.com>
2022-08-12 21:20:23 +0900

    Change mongourl

--- config/key.js
+++ config/key.js
@@ -1,6 +1,7 @@
 module.exports = {
-  MONGO_URL: 'mongodb://localhost:27017/',
+  //   MONGO_URL: 'mongodb://localhost:27017/',
   // MONGO_URL: "mongodb+srv://Edgars714:gYrm7fxC0xcdfFyo@cluster0.swkkimq.mongodb.net/mint",
-  // MONGO_URL: "mongodb+srv://DreamDevFast:dream.dev1215@cluster0.79xlk.mongodb.net/?retryWrites=true&w=majority",
+  MONGO_URL:
+    'mongodb+srv://DreamDevFast:dream.dev1215@cluster0.79xlk.mongodb.net/?retryWrites=true&w=majority',
   SECRET: 'jwt-secret',
 }
